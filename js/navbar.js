c9248b06a04fcf5bbef1765fef311b6fc6d08359
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-08 23:18:44 -0600

    Finally got the nav bar good.

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,5 @@
 // I'm gonna use some javascript on the menu for small screens!
-let menu = document.getElementById("menu");
-// let bool = false;
-let bool = true;
 document.getElementById('menu').addEventListener('click', (e) => {
     console.log(e.target.nextElementSibling.classList.toggle('hidden'));
 });
 
-// console.log(menu);
-// menu.style.visibility = "visible";
