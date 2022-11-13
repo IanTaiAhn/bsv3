2413ba547f11cda202165ce770b5c2ad6967b41b
IanTaiAhn <iantaiahn@yahoo.com>
2022-11-13 13:04:44 -0700

    Doing some nav bar stuff.

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,8 @@
 // I'm gonna use some javascript on the menu for small screens!
-document.getElementById('menu').addEventListener('click', (e) => {
-    console.log(e.target.nextElementSibling.classList.toggle('hidden'));
-});
+document.getElementById("menu").addEventListener("click", (e) => {
+  console.log(e.target.nextElementSibling.classList.toggle("hidden"));
+  e.target.nextElementSibling.classList.add("text-blue-400");
+  //   e.target.nextElementSibling.classList.add("");
 
+  console.log(e.target.nextElementSibling.classList);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html", "./js/*.js"],
   theme: {
     screens: {
-      xs: '260px',
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      xs: "260px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       keyframes: {
         growDiv: {
-          '0%': { transform: 'translateX(-100%)'},
-          '100%': { transform: 'translateX(0)'},          
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
         },
         growDivMidTRight: {
-          '0%': { transform: 'translateX(0)', width: '50%'},
-          '100%': { transform: 'translateX(100%)', width: '50%'},          
+          "0%": { transform: "translateX(0)", width: "50%" },
+          "100%": { transform: "translateX(100%)", width: "50%" },
         },
         growDivMidTLeft: {
-          '0%': { transform: 'translateX(0)', width: '50%'},
-          '100%': { transform: 'translateX(-100%)', width: '50%'},          
+          "0%": { transform: "translateX(0)", width: "50%" },
+          "100%": { transform: "translateX(-100%)", width: "50%" },
         },
         translateYandFade: {
-          '0%': { transform: 'translateY(-100%)', opacity: 0},
-          '100%': { transform: 'translateY(0)', opacity: 1},
+          "0%": { transform: "translateY(-100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
         },
         translateYandFadeFast: {
-          '0%': { transform: 'translateY(-100%)', opacity: 0},
-          '100%': { transform: 'translateY(0)', opacity: 1},
+          "0%": { transform: "translateY(-100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
         },
         translateFromRight: {
-          '0%': { transform: 'translateX(100%)'},
-          '100%': { transform: 'translateX(0)'},      
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
         },
         fadeIn: {
-          '0%': { opacity: 0},
-          '100%': { opacity: 1},      
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
         translateYBotToTop: {
-          '0%': { transform: 'translateY(0)', scale: 1},
-          '100%': { transform: 'translateY(-100%)', scale: 1.2},
+          "0%": { transform: "translateY(0)", scale: 1 },
+          "100%": { transform: "translateY(-100%)", scale: 1.2 },
         },
         fadeInSlow: {
-          '0%': { opacity: 0},
-          '100%': { opacity: 1},      
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
       animation: {
-        'divGrowRight': 'growDiv .4s linear',
-        'divGrowFromMidToRight': 'growDivMidTRight 1s linear',
-        'divGrowFromMidToLeft': 'growDivMidTLeft 1s linear',
-        'textFallnFade': 'translateYandFade 3s',
-        'textFallnFadeFast': 'translateYandFade 1.5s',
-        'translateFromRight': 'translateFromRight 1s',
-        'fadeIn': 'fadeIn 2s',
-        'fadeInSlow': 'fadeInSlow 4s',
-        'textGrow&Scale': 'translateYBotToTop forwards ease-in-out 1.5s',
-
+        divGrowRight: "growDiv .4s linear",
+        divGrowFromMidToRight: "growDivMidTRight 1s linear",
+        divGrowFromMidToLeft: "growDivMidTLeft 1s linear",
+        textFallnFade: "translateYandFade 3s",
+        textFallnFadeFast: "translateYandFade 1.5s",
+        translateFromRight: "translateFromRight 1s",
+        fadeIn: "fadeIn 2s",
+        fadeInSlow: "fadeInSlow 4s",
+        "textGrow&Scale": "translateYBotToTop forwards ease-in-out 1.5s",
       },
     },
   },
   plugins: [],
-}
+};
