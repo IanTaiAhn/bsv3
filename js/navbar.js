a64aa050648546aacfd9bcba615b881e536d3c44
IanTaiAhn <iantaiahn@yahoo.com>
2022-11-14 18:21:29 -0700

    Finally fixed the responsive hammy menu!

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,6 @@
 // I'm gonna use some javascript on the menu for small screens!
 document.getElementById("menu").addEventListener("click", (e) => {
-  console.log(e.target.nextElementSibling.classList.toggle("hidden"));
-  e.target.nextElementSibling.classList.add("text-blue-400");
-  //   e.target.nextElementSibling.classList.add("");
-
-  console.log(e.target.nextElementSibling.classList);
+  e.target.nextElementSibling.classList.toggle("translate-x-full");
+  e.target.nextElementSibling.classList.add("min-w-full");
+  e.target.nextElementSibling.classList.add("ease-out");
 });
