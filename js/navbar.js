eb002c702f64273dfeb40a52b0487e6b7e7b539b
IanTaiAhn <iantaiahn@mail.weber.edu>
2022-12-07 16:31:23 -0700

    Haha nope.

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,3 @@
-// I'm gonna use some javascript on the menu for small screens!
 document.getElementById("menu").addEventListener("click", (e) => {
   e.target.nextElementSibling.classList.toggle("translate-x-full");
   e.target.nextElementSibling.classList.add("min-w-full");
